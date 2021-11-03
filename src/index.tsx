8d1edfad931bb350fae70c4441080db9f88169e3
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-11-03 11:59:08 +0100

    basic form element

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const App = () => <h1>Hi!</h1>;
+const App = () => {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const onClick = () => {
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre></pre>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
