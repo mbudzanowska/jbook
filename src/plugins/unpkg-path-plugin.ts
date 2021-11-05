f6507a3efc35ad02fd182a88c0ba2b608985485d
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-11-05 11:11:19 +0100

    breaking up resolve logic with filters

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -10,13 +10,11 @@ export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
+      build.onResolve({ filter: /^index\.js$/ }, () => {
+        return { path: "index.js", namespace: "a" };
+      });
+
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResolve", args);
-
-        if (args.path === "index.js") {
-          return { path: args.path, namespace: "a" };
-        }
-
         if (args.path.includes("./") || args.path.includes("../")) {
           return {
             namespace: "a",
