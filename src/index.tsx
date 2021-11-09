69c35c7fcfa2873792ffc47322e3152a45160df4
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-11-09 11:48:54 +0100

    connecting react with redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import { store } from "./state";
+import { Provider } from "react-redux";
 // import CodeCell from "./components/code-cell";
 import TextEditor from "./components/text-editor";
 
 const App = () => {
   return (
-    <div>
-      {/* <CodeCell /> */}
-      <TextEditor/>
-    </div>
+    <Provider store={store}>
+      <div>
+        {/* <CodeCell /> */}
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -40,3 +40,5 @@ const reducer = (
     }
   }
 };
+
+export default reducer;
